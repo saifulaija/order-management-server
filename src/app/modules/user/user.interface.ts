1c3cfe7ee1b8863e6aa8100ba957d44446ea31db
saifulaija <saifulaija2023@gmail.com>
2023-11-23 00:03:39 +0600

    user type & schema are completed

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,18 @@
+export type TUser = {
+  userId: string;
+  userName: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+};
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>({
+  userId: { type: String, required: [true, 'Id is required'], unique: true },
+  userName: { type: String, required: [true, 'userName is required'] },
+  password: { type: String, required: [true, 'password is required'] },
+  fullName: {
+    firstName: { type: String, required: [true, 'firstName is required'] },
+    lastName: { type: String, required: [true, 'firstName is required'] },
+  },
+  age: { type: Number, required: [true, 'age is required'] },
+  email: { type: String, required: [true, 'email is required'] },
+  isActive: { type: Boolean },
+  hobbies: {
+    type: [String],
+    required: [true, 'hobbies is required'],
+    default: [],
+  },
+  address: {
+    street: { type: String, required: [true, 'street is required'] },
+    city: { type: String, required: [true, 'city is required'] },
+    country: { type: String, required: [true, 'country is required'] },
+  },
+});
+
+// 3. Create a Model.
+
+export const User = model<TUser>('User', userSchema);
